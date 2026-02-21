835b6f281eabf8e6ba8a366abc577f6db5b7c210
Michael Nouzieres <michael.nouzieres@gmail.com>
2026-02-21 09:52:09 -0500

    Revert "changing some title text"
    This reverts commit 596afacc630a70ea8cdc43799944156dc14ad931.

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
 
   return (
     <div style={{ fontFamily: "sans-serif", padding: 40 }}>
-      <h1>This is a fullstack dummy app</h1>
-      <h2>Using it to learn some DevOps</h2>
-      <h3>Let's stay in touch!</h3>
+      <h1>React + Express Fullstack</h1>
       <button onClick={handleClick}>Get the message</button>
       <p>{message}</p>
     </div>
